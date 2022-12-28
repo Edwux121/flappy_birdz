65ea5adf344acfdffe0c350a4ce0c2836a6dac64
Edvinas Dryžas <edvinas.dryzas@teltonika.lt>
2022-12-28 15:19:34 +0200

    Added movement to the bird, screen limitations and FPS limit

--- main.py
+++ main.py
@@ -24,16 +24,44 @@ class Main:
 
     def run_game(self):
         """Running the game"""
+        #Limiting the speed of the game
+        self.fps = 60
+        self.clock = pygame.time.Clock()
         while True:
-            #Watching for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            #Drawing display
-            self.screen.fill(self.settings.bg_color) 
-            self.bird.blitme()  
-            pygame.display.flip()
+            self.clock.tick(60)
+            self._check_events()
+            self._bird_movement()
+            self._update_screen()
 
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self._bird_movement_add()
+
+    def _update_screen(self):
+        """Updating the screen"""
+        #Drawing display
+        self.screen.fill(self.settings.bg_color) 
+        self.bird.blitme()  
+        pygame.display.flip()
+
+    def _bird_movement(self):
+        """Function for bird movement"""
+        #Get dimensions of the screen
+        self.screen_info = pygame.display.Info()
+        self.screen_width = self.screen_info.current_w
+        self.screen_height = self.screen_info.current_h
+
+        self.bird.rect.y += 2
+        #Checking if the Bird has reached the Bottom of the screen
+        self.bird.rect.clamp_ip(pygame.Rect(0, 0, self.screen_width, self.screen_height))
+
+    def _bird_movement_add(self):
+        """After pressing the spacebar makes the bird go up"""
+        self.bird.rect.y -= 100
 
 if __name__ == '__main__':
     #Make a game instance, and run the game.
